fc73d27fae1ebf8e55f3c12cd29b4ff8f710a61f
Vinay846 <vinayvin84629@gmail.com>
2021-07-29 00:55:46 +0530

    Solved

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
 
+import React, { useState } from "react";
+import "./../styles/Child.css";
+
+const Selection = (props) => {
+  const [nextBackground, selectNextBackground] = useState({ background: "" });
+
+  return (
+    <div
+      style={{ background: nextBackground.background }}
+      className={"fix-box"}
+      onClick={() => props.applyColor(selectNextBackground)}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+export default Selection;
